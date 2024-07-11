bc92d56035de57cebb277797d459e33bf05ca50e
alexr <alex.rodikov@audiocodes.com>
2024-07-11 19:53:06 +0300

    some additional config to run waitress server

--- configuration.py
+++ configuration.py
@@ -18,8 +18,9 @@ class Config:
     ALLOW_AUDC_PLUGIN: bool = False
     START_WEB_APP: bool = True
     web_app_port: int = 5000
-    scanner_app_name: str = 'netscan_app'
     web_app_name: str = 'flask_app'
+    web_server_app_path: str  = 'web_app/waitress_server.py'
+    scanner_app_name: str = 'netscan_app'
     log_files_path: Path = Path(__file__).parent / 'logs'
     log_files_path.mkdir(exist_ok=True)
     tmp_folder_path: Path = Path(__file__).parent / 'tmp'
--- web_app/waitress_server.py
+++ web_app/waitress_server.py
@@ -1,3 +1,15 @@
 from waitress import serve
+import sys
+import logging
+from pathlib import Path
+base_dir = str(Path(__file__).parent.parent)
+if base_dir not in sys.path: sys.path.append(base_dir)
+from configuration import Config
 import flask_web
-serve(flask_web.app, host='0.0.0.0', port=5000)
+
+if __name__ == '__main__':
+    logger = logging.getLogger('waitress_server')
+    try:
+        serve(flask_web.app, host='0.0.0.0', port=Config.web_app_port, threads=4)
+    except Exception as err:
+        logger.exception('An exception happened during running Flask app', exc_info=err)
